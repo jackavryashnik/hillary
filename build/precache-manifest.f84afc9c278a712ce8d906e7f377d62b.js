e202892fd17a6654cf2d8e3e6908655183c71cf7
jackavryashnik <jackavryashnik@gmail.com>
2023-12-19 23:03:09 +0200

    upd

---
+++ build/precache-manifest.f84afc9c278a712ce8d906e7f377d62b.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "56ee18aee4a324eeb5c7",
+    "url": "/hillary/static/css/main.fad2be82.chunk.css"
+  },
+  {
+    "revision": "56ee18aee4a324eeb5c7",
+    "url": "/hillary/static/js/main.56ee18ae.chunk.js"
+  },
+  {
+    "revision": "056d9b6e41dddd9ffd18",
+    "url": "/hillary/static/css/1.a89514e7.chunk.css"
+  },
+  {
+    "revision": "056d9b6e41dddd9ffd18",
+    "url": "/hillary/static/js/1.056d9b6e.chunk.js"
+  },
+  {
+    "revision": "52dc2cd5636df93dc631",
+    "url": "/hillary/static/js/runtime~main.52dc2cd5.js"
+  },
+  {
+    "revision": "43b4a38106bde68a5ae2c5df18927849",
+    "url": "/hillary/static/media/favicon.43b4a381.ico"
+  },
+  {
+    "revision": "f5e3e2e3923817256ce57dfae77e8417",
+    "url": "/hillary/static/media/logo_mine_ua.f5e3e2e3.webp"
+  },
+  {
+    "revision": "3b33dca18d52ff2ea1683cb9ebaf5008",
+    "url": "/hillary/static/media/wayforpay.3b33dca1.png"
+  },
+  {
+    "revision": "6f2bc7738621f21360505118b416902a",
+    "url": "/hillary/static/media/bg_footer1.6f2bc773.psd"
+  },
+  {
+    "revision": "e072d571753829a9f0a69819c3988eff",
+    "url": "/hillary/static/media/bg_header1.e072d571.psd"
+  },
+  {
+    "revision": "d47d0288c099e5a301aafc269dc7f387",
+    "url": "/hillary/static/media/bg_header_1.d47d0288.png"
+  },
+  {
+    "revision": "0c6ef615a10105934e6bd2baadf2dfed",
+    "url": "/hillary/static/media/bg_header_2.0c6ef615.png"
+  },
+  {
+    "revision": "3a65e153e8150c4a9bbf34bb0c06a496",
+    "url": "/hillary/static/media/guasha+main.3a65e153.png"
+  },
+  {
+    "revision": "9502587ca847cc882d975f100929d0ab",
+    "url": "/hillary/static/media/main.9502587c.png"
+  },
+  {
+    "revision": "53ba888b3d9f402759343066239a7695",
+    "url": "/hillary/static/media/bg_header_1.53ba888b.png"
+  },
+  {
+    "revision": "3f70a9f8e1baea6e9273f8ab0887bd8c",
+    "url": "/hillary/static/media/bg_header_2.3f70a9f8.png"
+  },
+  {
+    "revision": "fae5c50bd10dcd10ade90b21f4d91e6d",
+    "url": "/hillary/static/media/guasha+main.fae5c50b.png"
+  },
+  {
+    "revision": "de854f68a53da5d3a25dc1d0613685d8",
+    "url": "/hillary/static/media/main.de854f68.png"
+  },
+  {
+    "revision": "e0141059f2c5f20c578090cbc0d27aea",
+    "url": "/hillary/static/media/1.e0141059.png"
+  },
+  {
+    "revision": "e6c0be38ec4afa86abb08e6491a45a4d",
+    "url": "/hillary/static/media/2.e6c0be38.png"
+  },
+  {
+    "revision": "5127b2edb34894114ecaa9dfb05e6260",
+    "url": "/hillary/static/media/3.5127b2ed.png"
+  },
+  {
+    "revision": "76d62bbe39af2a1c63944f86b3fac303",
+    "url": "/hillary/static/media/4.76d62bbe.png"
+  },
+  {
+    "revision": "cd58ad2f5d7e71800e301d56d27a4b61",
+    "url": "/hillary/static/media/bg_footer.cd58ad2f.jpg"
+  },
+  {
+    "revision": "12ed7d86664057e6a901e2799ff4a674",
+    "url": "/hillary/static/media/bg_header.12ed7d86.png"
+  },
+  {
+    "revision": "b679c72ac34b057207e8280a4c2ca933",
+    "url": "/hillary/static/media/effects_1.b679c72a.jpg"
+  },
+  {
+    "revision": "2dad15387297ebb8ace6886338ee9f83",
+    "url": "/hillary/static/media/effects_2.2dad1538.jpg"
+  },
+  {
+    "revision": "301f0f8f002baed95cf0518f881393f5",
+    "url": "/hillary/static/media/effects_3.301f0f8f.jpg"
+  },
+  {
+    "revision": "682fe949870bc19ca7f8e85a143a762c",
+    "url": "/hillary/static/media/effects_4.682fe949.jpg"
+  },
+  {
+    "revision": "81d9f696c94b71bd943037ba7d5a0d79",
+    "url": "/hillary/static/media/massage_1.81d9f696.jpg"
+  },
+  {
+    "revision": "b90d445e514cb64b96a121da3dbe3223",
+    "url": "/hillary/static/media/massage_2.b90d445e.jpg"
+  },
+  {
+    "revision": "0784f40addc1d463e79d9979b4911d3e",
+    "url": "/hillary/static/media/massage_3.0784f40a.jpg"
+  },
+  {
+    "revision": "5f56edfb8d05fc57a03e3575c0e4d7d3",
+    "url": "/hillary/static/media/massage_4.5f56edfb.jpg"
+  },
+  {
+    "revision": "02a5e7fe683d6a75328ba4f2826d2eeb",
+    "url": "/hillary/static/media/noname_1.02a5e7fe.jpg"
+  },
+  {
+    "revision": "8ccca3c0d20d13df226618f6a904b487",
+    "url": "/hillary/static/media/noname_2.8ccca3c0.jpg"
+  },
+  {
+    "revision": "6a7409b86399de731cce8c83a20ecd28",
+    "url": "/hillary/static/media/tutorial_1.6a7409b8.png"
+  },
+  {
+    "revision": "10c003b4ca4cd4f5712b4340e3256754",
+    "url": "/hillary/static/media/tutorial_2.10c003b4.png"
+  },
+  {
+    "revision": "633e7f6260e39efe67e548437fc414e3",
+    "url": "/hillary/static/media/tutorial_old.633e7f62.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/hillary/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/hillary/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/hillary/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/hillary/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "865bb17f973de6d26f9719c38be77a37",
+    "url": "/hillary/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.804f6e30ebd06d77b5167ba7e294dab2.js"
+  "/hillary/precache-manifest.f84afc9c278a712ce8d906e7f377d62b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/hillary/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
