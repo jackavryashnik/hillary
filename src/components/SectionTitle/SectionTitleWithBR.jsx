516db42aeb101bc174f6d1a565109623b2492fdf
jackavryashnik <jackavryashnik@gmail.com>
2024-01-02 20:47:58 +0200

    fix inner tag

--- src/components/SectionTitle/SectionTitleWithBR.jsx
+++ src/components/SectionTitle/SectionTitleWithBR.jsx
@@ -6,11 +6,11 @@ const SectionTitleWithBR = ({ startText, boldText, endText, secondBoldText }) =>
         <div className="section-title flex__center">
             <p className="has-br">
                 {startText}
-                <div>
+                <span>
                     <b> {boldText} </b>
                     {endText}
                     <b> {secondBoldText} </b>
-                </div>
+                </span>
             </p>
         </div>
     );
